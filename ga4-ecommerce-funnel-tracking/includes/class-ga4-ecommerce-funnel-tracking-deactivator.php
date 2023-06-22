5667488984ac9a02933cd9c402b9ddcfea167530
chaudhari-mithilesh <mithilesh.chaudhari@wisdmlabs.com>
2023-06-22 15:07:41 +0530

    Initial Commit. Added plugin boilerplate.

---
+++ ga4-ecommerce-funnel-tracking/includes/class-ga4-ecommerce-funnel-tracking-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wisdmalbs.com
+ * @since      1.0.0
+ *
+ * @package    Ga4_Ecommerce_Funnel_Tracking
+ * @subpackage Ga4_Ecommerce_Funnel_Tracking/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Ga4_Ecommerce_Funnel_Tracking
+ * @subpackage Ga4_Ecommerce_Funnel_Tracking/includes
+ * @author     WisdmLabs <software@wisdmlabs.com>
+ */
+class Ga4_Ecommerce_Funnel_Tracking_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
